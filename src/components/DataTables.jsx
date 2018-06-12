292f03baa2697bfc430cf5fb13f154008aa33707
l2jong <gkwkdlawoh@naver.com>
2018-06-12 16:57:01 +0900

    customer, Consultant 칼럼 추가 axios 활용 json-server 3001포트

--- src/components/DataTables.jsx
+++ src/components/DataTables.jsx
@@ -1,275 +1,293 @@
 /*
  * Created by dkel on 2018-06-09.
  */
-import React, { Component } from 'react';
-import { DataTable } from 'react-data-components';
-import ColumnList from '../columnList.json';
+import React, {Component} from 'react';
+import {DataTable} from 'react-data-components';
+import axios from 'axios';
 
 class Contents extends Component {
-  constructor(props) {
-    super(props);
 
-    /*	this.renderMapUrl = this.renderMapUrl.bind(this);
-      this.clickItem = this.clickItem.bind(this);*/
-  }
+	state = {
+		columnList: []
+	};
 
-  /** @namespace ColumnList.Cleanings */
-  render() {
-    const columns = ColumnList.Cleanings;
+	constructor(props) {
+		super(props);
 
-    const data = [
-      {
-        id: 'gkwkdlawoh',
-        name: 'Lee',
-        customer_id: 'gkwkdlawoh',
-        city: 'Suwon',
-        district: 'Gyeonggi-do',
-        detailed_address: '321-32',
-        contact: '010-3227-4617',
-        date: '2016-02-17',
-        home_type: 'APT',
-        size: 70,
-        customer_type: 'B2B',
-        cleaning_type: '정기',
-        status_of_subscription: '2/17',
-        tool: 'with tool',
-        rank_of_cleaning: 3,
-        bathroom_cleaning: 'O',
-        kitchen_cleaning: 'X',
-        msc_cleaning: 'X',
-        window_cleaning: 'X',
-        balcony_cleaning: 'X',
-        refrigerator_cleaning: 'X',
-        ironing_cleaning: 'X',
-        ceiling_cleaning: 'X',
-        cleaning_status: 'Not yet',
-        payment_status: 'paid',
-        transaction_id: '01237468',
-        consultant_status: 'matched',
-        payment: 280000,
-        price: 320000,
-        discount: 40000,
-        used_point: 20000,
-        discounted_promotion: 20000,
-        promo_content: '10-Subscription(10%)',
-        status_of_start: 'late',
-        total_expected_duration: 6,
-        expected_duration: 3,
-        total_real_duration: 0,
-        real_duration: 0,
-        each_consultant_salary: 100000,
-        total_consultant_salary: 200000,
-        payment_type: 'VA',
-        using_app: 'App',
-        tip_for_each: 10000,
-        total_tip: 20000,
-        request_detail: 'Focus on bathroom',
-        number_of_consultants: 2,
-        consultants_name: 'Sri, Ira',
-        cleaning_rate_done: 'Y',
-        cleaning_rate: 4,
-        cleaning_feedback1: '청소도구가 불만이에요',
-        cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
-        insert_date: '2052018',
-        bed_rooms: 1,
-        bath_rooms: 1,
-      },
-      {
-        id: 'gkwkdlawoa',
-        name: 'Lee',
-        customer_id: 'gkwkdlawoh',
-        city: 'Suwon',
-        district: 'Gyeonggi-do',
-        detailed_address: '321-32',
-        contact: '010-3227-4617',
-        date: '2016-02-17',
-        home_type: 'APT',
-        size: 70,
-        customer_type: 'B2B',
-        cleaning_type: '정기',
-        status_of_subscription: '2/17',
-        tool: 'with tool',
-        rank_of_cleaning: 3,
-        bathroom_cleaning: 'O',
-        kitchen_cleaning: 'X',
-        msc_cleaning: 'X',
-        window_cleaning: 'X',
-        balcony_cleaning: 'X',
-        refrigerator_cleaning: 'X',
-        ironing_cleaning: 'X',
-        ceiling_cleaning: 'X',
-        cleaning_status: 'Not yet',
-        payment_status: 'paid',
-        transaction_id: '01237468',
-        consultant_status: 'matched',
-        payment: 280000,
-        price: 320000,
-        discount: 40000,
-        used_point: 20000,
-        discounted_promotion: 20000,
-        promo_content: '10-Subscription(10%)',
-        status_of_start: 'late',
-        total_expected_duration: 6,
-        expected_duration: 3,
-        total_real_duration: 0,
-        real_duration: 0,
-        each_consultant_salary: 100000,
-        total_consultant_salary: 200000,
-        payment_type: 'VA',
-        using_app: 'App',
-        tip_for_each: 10000,
-        total_tip: 20000,
-        request_detail: 'Focus on bathroom',
-        number_of_consultants: 2,
-        consultants_name: 'Sri, Ira',
-        cleaning_rate_done: 'Y',
-        cleaning_rate: 4,
-        cleaning_feedback1: '청소도구가 불만이에요',
-        cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
-        insert_date: '2052018',
-        bed_rooms: 1,
-        bath_rooms: 1,
-      },
-      {
-        id: 'gkwkdlawod',
-        name: 'Lee',
-        customer_id: 'gkwkdlawoh',
-        city: 'Suwon',
-        district: 'Gyeonggi-do',
-        detailed_address: '321-32',
-        contact: '010-3227-4617',
-        date: '2016-02-17',
-        home_type: 'APT',
-        size: 70,
-        customer_type: 'B2B',
-        cleaning_type: '정기',
-        status_of_subscription: '2/17',
-        tool: 'with tool',
-        rank_of_cleaning: 3,
-        bathroom_cleaning: 'O',
-        kitchen_cleaning: 'X',
-        msc_cleaning: 'X',
-        window_cleaning: 'X',
-        balcony_cleaning: 'X',
-        refrigerator_cleaning: 'X',
-        ironing_cleaning: 'X',
-        ceiling_cleaning: 'X',
-        cleaning_status: 'Not yet',
-        payment_status: 'paid',
-        transaction_id: '01237468',
-        consultant_status: 'matched',
-        payment: 280000,
-        price: 320000,
-        discount: 40000,
-        used_point: 20000,
-        discounted_promotion: 20000,
-        promo_content: '10-Subscription(10%)',
-        status_of_start: 'late',
-        total_expected_duration: 6,
-        expected_duration: 3,
-        total_real_duration: 0,
-        real_duration: 0,
-        each_consultant_salary: 100000,
-        total_consultant_salary: 200000,
-        payment_type: 'VA',
-        using_app: 'App',
-        tip_for_each: 10000,
-        total_tip: 20000,
-        request_detail: 'Focus on bathroom',
-        number_of_consultants: 2,
-        consultants_name: 'Sri, Ira',
-        cleaning_rate_done: 'Y',
-        cleaning_rate: 4,
-        cleaning_feedback1: '청소도구가 불만이에요',
-        cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
-        insert_date: '2052018',
-        bed_rooms: 1,
-        bath_rooms: 1,
-      },
-      {
-        id: 'l2jong',
-        name: 'Lee',
-        customer_id: 'gkwkdlawoh',
-        city: 'Suwon',
-        district: 'Gyeonggi-do',
-        detailed_address: '321-32',
-        contact: '010-3227-4617',
-        date: '2016-02-17',
-        home_type: 'APT',
-        size: 70,
-        customer_type: 'B2B',
-        cleaning_type: '정기',
-        status_of_subscription: '2/17',
-        tool: 'with tool',
-        rank_of_cleaning: 3,
-        bathroom_cleaning: 'O',
-        kitchen_cleaning: 'X',
-        msc_cleaning: 'X',
-        window_cleaning: 'X',
-        balcony_cleaning: 'X',
-        refrigerator_cleaning: 'X',
-        ironing_cleaning: 'X',
-        ceiling_cleaning: 'X',
-        cleaning_status: 'Not yet',
-        payment_status: 'paid',
-        transaction_id: '01237468',
-        consultant_status: 'matched',
-        payment: 280000,
-        price: 320000,
-        discount: 40000,
-        used_point: 20000,
-        discounted_promotion: 20000,
-        promo_content: '10-Subscription(10%)',
-        status_of_start: 'late',
-        total_expected_duration: 6,
-        expected_duration: 3,
-        total_real_duration: 0,
-        real_duration: 0,
-        each_consultant_salary: 100000,
-        total_consultant_salary: 200000,
-        payment_type: 'VA',
-        using_app: 'App',
-        tip_for_each: 10000,
-        total_tip: 20000,
-        request_detail: 'Focus on bathroom',
-        number_of_consultants: 2,
-        consultants_name: 'Sri, Ira',
-        cleaning_rate_done: 'Y',
-        cleaning_rate: 4,
-        cleaning_feedback1: '청소도구가 불만이에요',
-        cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
-        insert_date: '2052018',
-        bed_rooms: 1,
-        bath_rooms: 1,
-      },
+		/*	this.renderMapUrl = this.renderMapUrl.bind(this);
+		 this.clickItem = this.clickItem.bind(this);*/
+		axios.get('http://localhost:3001/Cleanings')
+			.then(response => {
+				console.log(response);
+				response.data[0].render = this.renderMapUrl;
+				this.setState({
+					columnList: response.data
+				});
+			}) // SUCCESS
+			.catch(response => {
+				console.log(response);
+			}); // ERROR
+	}
 
-      // It also supports arrays
-      // [ 'name value', 'city value', 'address value', 'phone value' ]
-    ];
+	/** @namespace ColumnList.Cleanings */
+	render() {
 
-    columns[0].render = this.renderMapUrl;
+		const data = [
+			{
+				id: 'gkwkdlawoh',
+				name: 'Lee',
+				customer_id: 'gkwkdlawoh',
+				city: 'Suwon',
+				district: 'Gyeonggi-do',
+				detailed_address: '321-32',
+				nationality: 'South Korea',
+				contact: '010-3227-4617',
+				date: '2016-02-17',
+				home_type: 'APT',
+				size: 70,
+				customer_type: 'B2B',
+				cleaning_type: '정기',
+				status_of_subscription: '2/17',
+				tool: 'with tool',
+				rank_of_cleaning: 3,
+				bathroom_cleaning: 'O',
+				kitchen_cleaning: 'X',
+				msc_cleaning: 'X',
+				window_cleaning: 'X',
+				balcony_cleaning: 'X',
+				refrigerator_cleaning: 'X',
+				ironing_cleaning: 'X',
+				ceiling_cleaning: 'X',
+				cleaning_status: 'Not yet',
+				payment_status: 'paid',
+				transaction_id: '01237468',
+				consultant_status: 'matched',
+				payment: 280000,
+				price: 320000,
+				discount: 40000,
+				used_point: 20000,
+				discounted_promotion: 20000,
+				promo_content: '10-Subscription(10%)',
+				status_of_start: 'late',
+				total_expected_duration: 6,
+				expected_duration: 3,
+				total_real_duration: 0,
+				real_duration: 0,
+				each_consultant_salary: 100000,
+				total_consultant_salary: 200000,
+				payment_type: 'VA',
+				using_app: 'App',
+				tip_for_each: 10000,
+				total_tip: 20000,
+				request_detail: 'Focus on bathroom',
+				number_of_consultants: 2,
+				consultants_name: 'Sri, Ira',
+				cleaning_rate_done: 'Y',
+				cleaning_rate: 4,
+				cleaning_feedback1: '청소도구가 불만이에요',
+				cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
+				insert_date: '2052018',
+				bed_rooms: 1,
+				bath_rooms: 1,
+			},
+			{
+				id: 'gkwkdlawoa',
+				name: 'Lee',
+				customer_id: 'gkwkdlawoh',
+				city: 'Suwon',
+				district: 'Gyeonggi-do',
+				detailed_address: '321-32',
+				nationality: 'South Korea',
+				contact: '010-3227-4617',
+				date: '2016-02-17',
+				home_type: 'APT',
+				size: 70,
+				customer_type: 'B2B',
+				cleaning_type: '정기',
+				status_of_subscription: '2/17',
+				tool: 'with tool',
+				rank_of_cleaning: 3,
+				bathroom_cleaning: 'O',
+				kitchen_cleaning: 'X',
+				msc_cleaning: 'X',
+				window_cleaning: 'X',
+				balcony_cleaning: 'X',
+				refrigerator_cleaning: 'X',
+				ironing_cleaning: 'X',
+				ceiling_cleaning: 'X',
+				cleaning_status: 'Not yet',
+				payment_status: 'paid',
+				transaction_id: '01237468',
+				consultant_status: 'matched',
+				payment: 280000,
+				price: 320000,
+				discount: 40000,
+				used_point: 20000,
+				discounted_promotion: 20000,
+				promo_content: '10-Subscription(10%)',
+				status_of_start: 'late',
+				total_expected_duration: 6,
+				expected_duration: 3,
+				total_real_duration: 0,
+				real_duration: 0,
+				each_consultant_salary: 100000,
+				total_consultant_salary: 200000,
+				payment_type: 'VA',
+				using_app: 'App',
+				tip_for_each: 10000,
+				total_tip: 20000,
+				request_detail: 'Focus on bathroom',
+				number_of_consultants: 2,
+				consultants_name: 'Sri, Ira',
+				cleaning_rate_done: 'Y',
+				cleaning_rate: 4,
+				cleaning_feedback1: '청소도구가 불만이에요',
+				cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
+				insert_date: '2052018',
+				bed_rooms: 1,
+				bath_rooms: 1,
+			},
+			{
+				id: 'gkwkdlawod',
+				name: 'Lee',
+				customer_id: 'gkwkdlawoh',
+				city: 'Suwon',
+				district: 'Gyeonggi-do',
+				detailed_address: '321-32',
+				nationality: 'South Korea',
+				contact: '010-3227-4617',
+				date: '2016-02-17',
+				home_type: 'APT',
+				size: 70,
+				customer_type: 'B2B',
+				cleaning_type: '정기',
+				status_of_subscription: '2/17',
+				tool: 'with tool',
+				rank_of_cleaning: 3,
+				bathroom_cleaning: 'O',
+				kitchen_cleaning: 'X',
+				msc_cleaning: 'X',
+				window_cleaning: 'X',
+				balcony_cleaning: 'X',
+				refrigerator_cleaning: 'X',
+				ironing_cleaning: 'X',
+				ceiling_cleaning: 'X',
+				cleaning_status: 'Not yet',
+				payment_status: 'paid',
+				transaction_id: '01237468',
+				consultant_status: 'matched',
+				payment: 280000,
+				price: 320000,
+				discount: 40000,
+				used_point: 20000,
+				discounted_promotion: 20000,
+				promo_content: '10-Subscription(10%)',
+				status_of_start: 'late',
+				total_expected_duration: 6,
+				expected_duration: 3,
+				total_real_duration: 0,
+				real_duration: 0,
+				each_consultant_salary: 100000,
+				total_consultant_salary: 200000,
+				payment_type: 'VA',
+				using_app: 'App',
+				tip_for_each: 10000,
+				total_tip: 20000,
+				request_detail: 'Focus on bathroom',
+				number_of_consultants: 2,
+				consultants_name: 'Sri, Ira',
+				cleaning_rate_done: 'Y',
+				cleaning_rate: 4,
+				cleaning_feedback1: '청소도구가 불만이에요',
+				cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
+				insert_date: '2052018',
+				bed_rooms: 1,
+				bath_rooms: 1,
+			},
+			{
+				id: 'l2jong',
+				name: 'Lee',
+				customer_id: 'gkwkdlawoh',
+				city: 'Suwon',
+				district: 'Gyeonggi-do',
+				detailed_address: '321-32',
+				nationality: 'South Korea',
+				contact: '010-3227-4617',
+				date: '2016-02-17',
+				home_type: 'APT',
+				size: 70,
+				customer_type: 'B2B',
+				cleaning_type: '정기',
+				status_of_subscription: '2/17',
+				tool: 'with tool',
+				rank_of_cleaning: 3,
+				bathroom_cleaning: 'O',
+				kitchen_cleaning: 'X',
+				msc_cleaning: 'X',
+				window_cleaning: 'X',
+				balcony_cleaning: 'X',
+				refrigerator_cleaning: 'X',
+				ironing_cleaning: 'X',
+				ceiling_cleaning: 'X',
+				cleaning_status: 'Not yet',
+				payment_status: 'paid',
+				transaction_id: '01237468',
+				consultant_status: 'matched',
+				payment: 280000,
+				price: 320000,
+				discount: 40000,
+				used_point: 20000,
+				discounted_promotion: 20000,
+				promo_content: '10-Subscription(10%)',
+				status_of_start: 'late',
+				total_expected_duration: 6,
+				expected_duration: 3,
+				total_real_duration: 0,
+				real_duration: 0,
+				each_consultant_salary: 100000,
+				total_consultant_salary: 200000,
+				payment_type: 'VA',
+				using_app: 'App',
+				tip_for_each: 10000,
+				total_tip: 20000,
+				request_detail: 'Focus on bathroom',
+				number_of_consultants: 2,
+				consultants_name: 'Sri, Ira',
+				cleaning_rate_done: 'Y',
+				cleaning_rate: 4,
+				cleaning_feedback1: '청소도구가 불만이에요',
+				cleaning_feedback2: '진공청소기 상태가 좋아보이지 않았어요',
+				insert_date: '2052018',
+				bed_rooms: 1,
+				bath_rooms: 1,
+			},
 
-    return (
-      <section className="right">
-        <h3 className="title">{this.props.defaultMenu}</h3>
-        <DataTable
-          keys={columns[0].prop}
-          columns={columns}
-          initialData={data}
-          initialPageLength={5}
-          initialSortBy={{ prop: columns[0].prop, order: 'descending' }}
-          table=""
-        />
-      </section>
-    );
-  }
-  renderMapUrl = (val, row) => (
-    <a /*onClick={this.clickItem('gigigi')}*/>{val}</a>
-  );
+			// It also supports arrays
+			// [ 'name value', 'city value', 'address value', 'phone value' ]
+		];
 
-  /*clickItem = (value) => {
-		alert(value);
-	}*/
+		return (
+			<section className="right">
+				<h3 className="title">{this.props.defaultMenu}</h3>
+				<DataTable
+					keys={"id"}
+					columns={this.state.columnList}
+					initialData={data}
+					initialPageLength={5}
+					initialSortBy={{prop: "id", order: 'descending'}}
+					table=""
+				/>
+			</section>
+		);
+	}
+
+	renderMapUrl = (val, row) => (
+		<a /*onClick={this.clickItem('gigigi')}*/>{val}</a>
+	);
+
+	/*clickItem = (value) => {
+	 alert(value);
+	 }*/
 }
 
 export default Contents;
